c6d18e9ad344e927a68fe18d8c2fe8b9ce9314ba
curry7steven <438560027@qq.com>
2020-09-08 16:40:49 +0800

    输出修改正确，验证推送

--- src/src/huawei0908.java
+++ src/src/huawei0908.java
@@ -1,6 +1,12 @@
 package src;
 
 public class huawei0908 {
-    //1231231
-    System.out.println()
+
+
+    public  static  void main(String[] args){
+        //我修改了正确的输出
+        System.out.printlin;
+    }
+
+
 }
